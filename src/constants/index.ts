37e18b365a848e1aad548b838a5cbaffefe3cf92
phong phan <phongphanq089@gmail.com>
2026-05-20 16:03:14 +0700

    feat : update domain call api

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,5 @@
 export const DEFAULT_FOLDER_MEDIA = 'all'
-export const API_BASE_URL =
-  'https://ecommerce-fashion-be-phongphanq0894143-npyksiu6.leapcell.dev/api'
+export const API_BASE_URL = 'https://my-ecommerce-be.onrender.com/api'
 
 export const PHONE_REGEX = new RegExp(
   /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/,
