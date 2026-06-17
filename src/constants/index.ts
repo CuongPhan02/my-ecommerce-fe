27d5cde3fe45541e6c2696d00c88fdf176def7de
CuongPM2004 <phanmanhcuong2k4thptnt@gmail.com>
2026-06-17 23:53:39 +0700

    fix: use environment variables for API and Redirect URLs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,13 @@
 export const DEFAULT_FOLDER_MEDIA = 'all'
 // export const API_BASE_URL = 'https://my-ecommerce-be.onrender.com/api'
-export const API_BASE_URL = 'http://127.0.0.1:3000/api'
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:3000/api'
 
 export const PHONE_REGEX = new RegExp(
   /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/,
 )
 
 export const SETTING_AUTH = {
-  URL_REDIRECT: 'http://localhost:3000/auth',
+  URL_REDIRECT: process.env.NEXT_PUBLIC_URL_REDIRECT || 'http://localhost:3000/auth',
 }
 
 export const GOOGLE_CLIENT_ID =
